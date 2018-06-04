757b1aeba63046d463456a51510354b785e0edb5
Krzysztof Platis <platonn.git@gmail.com>
2018-06-04 22:32:12 +0200

    extract mocked data

---
+++ src/app/mocked-data/tasks.ts
@@ -0,0 +1,52 @@
+import * as moment from 'moment';
+
+const momentZero = moment(0).utc();
+
+export const tasks = [
+    {
+      id: '1',
+      name: 'task 1',
+      durationMinutes: 15,
+      durationHours: 0,
+      endTime: momentZero,
+      plannedEndTime: momentZero,
+      timeLeft: momentZero
+    },
+    {
+      id: '2',
+      name: 'task 2',
+      durationMinutes: 30,
+      durationHours: 0,
+      endTime: momentZero,
+      plannedEndTime: momentZero,
+      timeLeft: momentZero
+    },
+    {
+      id: '3',
+      name: 'task 3',
+      durationMinutes: 30,
+      durationHours: 0,
+      endTime: momentZero,
+      plannedEndTime: momentZero,
+      timeLeft: momentZero
+    },
+    {
+      id: '4',
+      name: 'task 4',
+      durationMinutes: 30,
+      durationHours: 0,
+      endTime: momentZero,
+      plannedEndTime: momentZero,
+      timeLeft: momentZero
+    },
+    {
+      id: '5',
+      name: 'task 5',
+      durationMinutes: 30,
+      durationHours: 0,
+      endTime: momentZero,
+      plannedEndTime: momentZero,
+      timeLeft: momentZero
+    }
+
+  ];
--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Task} from '../models'
-import * as moment from 'moment';
-
+import {tasks} from '../mocked-data'
 
 @Injectable()
 export class TasksService {
@@ -10,55 +9,7 @@ export class TasksService {
   }
 
   public getTasks(): Task[] {
-    let momentZero = moment(0).utc();
-    return [
-      {
-        id: '1',
-        name: 'task 1',
-        durationMinutes: 15,
-        durationHours: 0,
-        endTime: momentZero,
-        plannedEndTime: momentZero,
-        timeLeft: momentZero
-      },
-      {
-        id: '2',
-        name: 'task 2',
-        durationMinutes: 30,
-        durationHours: 0,
-        endTime: momentZero,
-        plannedEndTime: momentZero,
-        timeLeft: momentZero
-      },
-      {
-        id: '3',
-        name: 'task 3',
-        durationMinutes: 30,
-        durationHours: 0,
-        endTime: momentZero,
-        plannedEndTime: momentZero,
-        timeLeft: momentZero
-      },
-      {
-        id: '4',
-        name: 'task 4',
-        durationMinutes: 30,
-        durationHours: 0,
-        endTime: momentZero,
-        plannedEndTime: momentZero,
-        timeLeft: momentZero
-      },
-      {
-        id: '5',
-        name: 'task 5',
-        durationMinutes: 30,
-        durationHours: 0,
-        endTime: momentZero,
-        plannedEndTime: momentZero,
-        timeLeft: momentZero
-      }
-
-    ];
+    return tasks;
   }
 
 }
